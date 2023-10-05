2a6c9d790d0f22eacbf7259893a085e8fc94f1b9
SooHoon <dkehskeh@gmail.com>
2023-10-05 13:34:01 +0900

    10/4

---
+++ 백준/10. 별찍기/10. 별찍기/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  10. 별찍기
+//
+//  Created by 최수훈 on 2023/10/04.
+//
+
+import Foundation
+
+var input = Int(readLine()!)!
+var result = ""
+
+for i in 0..<input {
+    result += "*"
+    print(result)
+}
