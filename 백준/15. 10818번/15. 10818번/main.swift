df82114c9ade7dbb1745141e9ca99f6bc03e65ce
SooHoon <dkehskeh@gmail.com>
2023-10-10 15:15:36 +0900

    10/10

---
+++ 백준/15. 10818번/15. 10818번/main.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  15. 10818번
+//
+//  Created by 최수훈 on 2023/10/10.
+//
+
+import Foundation
+
+//let count = Int(readLine()!)!
+//let numbers : [Int] = readLine()!.split(separator: " ").map { Int($0)! }
+//
+//print(numbers.min()!, numbers.max()!)
+
+
+let component = readLine()!.components(separatedBy: " ")
+let split = readLine()!.split(separator: " ")
+
+print(type(of: component), type(of: split))
